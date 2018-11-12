a2539994947357e4f1d2d527dcbe181af1992904
564742+liquidkode@users.noreply.github.com <564742+liquidkode@users.noreply.github.com>
2018-11-12 15:50:32 +0000

    API

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,13 +1,21 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class GreetingController {
+
+    @Autowired
+    private Environment env;
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -19,6 +27,15 @@ public class GreetingController {
 
     @RequestMapping(value = "/version", method = RequestMethod.GET)
     public String version() {
-        return new String("0");
+	return new String("reading env variables 1");
     }
+
+    @RequestMapping(value = "/env", method = RequestMethod.GET)
+    public String env() {
+
+	String s = "Client Id: " + env.getProperty("foursquare-client-id")
+	+ " Client Secret: " + env.getProperty("foursquare-client-secret");
+	return s;
+    }
+
 }
